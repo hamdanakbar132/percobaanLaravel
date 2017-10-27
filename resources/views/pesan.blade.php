97ed3b4c46a693740a55316a2d3355a6c48cd15e
hamdanakbar132 <hamdanakbar132@gmail.com>
2017-10-27 14:57:32 +0700

    tambah

--- resources/views/pesan.blade.php
+++ resources/views/pesan.blade.php
@@ -79,12 +79,17 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Belajar Laravel
+                    Pesan
                 </div>
-
-                <div class="links">
-                    <a href="pesan">Pesan</a>
-                    <a href="about">About</a>
+				{!! Form::open(array('url' =>
+				'pesan/kirim','method'=>'POST')) !!}
+                <div>
+                   <strong>Pesan:</strong>
+				   {!! Form::text('pesan', null,
+				   array('placeholder' => 'Tuliskan
+				   pesan','class' =>'form-control')) !!}
+				   <button type="submit" class="btn
+				   btn-primary">kirim</button>
                 </div>
             </div>
         </div>
